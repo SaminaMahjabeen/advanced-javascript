59a085467fb51535c5b68c719eb4ab87d0a462d6
SaminaMahjabeen <samina.hccd@gmail.com>
2020-08-07 02:57:11 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// truthy
+// '0',' ',[]
+// falsy
+// false,0,"",undefined,null,NaN
+  const name=[];
+ console.log(name);
+ if(name){
+     console.log("condition is true");
+ }
+ else   
+  console.log("condition is false");
+
+ let a=NaN;
+console.log(a);
+if(a){
+    console.log("condition is true");
+}
+else   
+ console.log("condition is false");
