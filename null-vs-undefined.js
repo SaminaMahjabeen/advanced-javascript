f2577b31022c04403814e679023c1da4683e1964
SaminaMahjabeen <samina.hccd@gmail.com>
2020-08-07 11:43:43 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+// undefined
+// 1
+function add(num1,num2){
+    console.log(num1,num2);
+}
+const a=add(3,4);
+console.log(a);
+// 2
+function add(num1,num2){
+    console.log(num1,num2);
+}
+const b=add(3);
+console.log(b);
+// 3
+const arr=[3,4,6];
+console.log(arr[5]);
+// 4
+const df={name:"samina",phone:344
+};
+console.log(df.address);
